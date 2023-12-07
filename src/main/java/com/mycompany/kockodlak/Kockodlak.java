bb249264b5b0e7d8400c8b3695f59207046142da
Joukl Jakub <st64134@upce.cz>
2023-12-07 16:11:08 +0100

    Zmeny

--- src/main/java/com/mycompany/kockodlak/Kockodlak.java
+++ src/main/java/com/mycompany/kockodlak/Kockodlak.java
@@ -13,4 +13,8 @@ public class Kockodlak {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public static int secti(int a, int b){
+        return a + b;
+    }
 }
---
+++ src/test/java/com/mycompany/kockodlak/KockodlakTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.kockodlak;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author st64134
+ */
+public class KockodlakTest {
+    
+    public KockodlakTest() {
+    }
+
+    /**
+     * Test of main method, of class Kockodlak.
+     */
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+    }
+
+    /**
+     * Test of secti method, of class Kockodlak.
+     */
+    @org.junit.jupiter.api.Test
+    public void testSecti() {
+        Assertions.fail();
+    }
+    
+}
